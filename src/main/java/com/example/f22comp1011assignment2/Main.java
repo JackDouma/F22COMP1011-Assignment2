2e4280cbf580f4dbd55dba1bae474e12a8b80511
JackDouma <jackdouma0@gmail.com>
2022-11-30 18:00:19 -0500

    logo

--- src/main/java/com/example/f22comp1011assignment2/Main.java
+++ src/main/java/com/example/f22comp1011assignment2/Main.java
@@ -3,6 +3,7 @@ package com.example.f22comp1011assignment2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,10 @@ public class Main extends Application
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("standings-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SportPal");
+
+        // from https://www.demo2s.com/java/javafx-stage-geticons.html
+        stage.getIcons().add(new Image(this.getClass().getResource("sportpal.png").toString()));
+
         stage.setScene(scene);
         stage.show();
     }
